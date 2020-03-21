0dc8a0c76e7a5996adb377b859711f5e34f7fdee
maximhesed <maximtemny@mail.ru>
2020-03-22 02:57:18 +0300

    optimization
    add argparse error interceptor
    remove links_count() and subs
    optimize crds_calc(), mut_*()
    remove pedantic mode
    add additional info to --ll-show

--- gen.py
+++ gen.py
@@ -4,60 +4,63 @@ import sys
 import time
 import argparse
 
-from math import sqrt
-
-def _opts_get(args=sys.argv[1:]):
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--elq", type=int, help="elements quantity")
-    parser.add_argument("--dx", type=int, help="cell's size by X")
-    parser.add_argument("--dy", type=int, help="cell's size by Y")
-    parser.add_argument("--nx", type=int, help="cells quantity by X")
-    parser.add_argument("--ny", type=int, help="cells quantity by Y")
-    parser.add_argument("--edq", type=int, help="edges quantity")
-    parser.add_argument("--edq-max", type=int,
-        help="max edges between elements")
-    parser.add_argument("--its", type=int, help="iterations quantity")
-    parser.add_argument("-f", "--fname", type=str, help="output file")
-    parser.add_argument("--with-sort", action="store_true",
-        help="sort elements positions")
-    parser.add_argument("--ll-show", action="store_true",
-        help="show sum of links lengths, if changed")
-    parser.add_argument("--with-swap", action="store_true",
-        help="swap random elements positions")
-    parser.add_argument("--mode", type=str,
-        help="hard: randomize all elements positions; " +
-            "soft: randomize one random element's position; " +
-            "pedantic: soft with storing changed position")
-    parser.add_argument("-c", "--comp-diagonal", action="store_true")
-
-    parser.set_defaults(with_sort=False)
-    parser.set_defaults(ll_show=False)
-    parser.set_defaults(with_swap=False)
-    parser.set_defaults(mode="hard")
-    parser.set_defaults(comp_diagonal=False)
-
-    opts, unknown = parser.parse_known_args(args)
-
-    if unknown:
+def _error(parser):
+    def wrapper(interceptor):
         parser.print_help()
 
         sys.exit(-1)
 
-    return parser, opts
+    return wrapper
+
+def _args_get(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+
+    parser.error = _error(parser)
+
+    parser.add_argument("elq", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("dx", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("dy", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("nx", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("ny", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("edq", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("edq_max", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("its", type=int, help=argparse.SUPPRESS)
+    parser.add_argument("fname", type=str, help=argparse.SUPPRESS)
+    parser.add_argument("--with-swap", action="store_true",
+        help="swap random elements positions")
+    parser.add_argument("--with-sort", action="store_true",
+        help="sort elements positions")
+    parser.add_argument("--ll-show", action="store_true",
+        help="show sum of links lengths, if changed")
+    parser.add_argument("--mode", type=str,
+        help="hard: randomize all elements positions, each iteration; " +
+            "soft: randomize one random element's position")
+    parser.add_argument("-c", "--comp-diagonal", action="store_true")
+
+    parser.set_defaults(with_swap=False)
+    parser.set_defaults(with_sort=False)
+    parser.set_defaults(ll_show=False)
+    parser.set_defaults(mode="hard")
+    parser.set_defaults(comp_diagonal=False)
+
+    args = parser.parse_args(args)
+
+    return parser, args
 
 def same(lst):
     for i in lst:
         if lst.count(i) > 1:
             print("same")
 
+            return
+
 def rand(left, right):
     v = int((right + 1 - left) * random.random() + left)
 
     return v
 
-def swap(lst, p1, p2):
-    lst[p1], lst[p2] = lst[p2], lst[p1]
+def swap(lst, e1, e2):
+    lst[e1], lst[e2] = lst[e2], lst[e1]
 
     return lst
 
@@ -75,41 +78,26 @@ def list_print(lst, step=4, ss=1):
 
     print("]")
 
-def dict_print(dct, step=4, ss=1):
-    print("{", end="")
+def edgs_gen(elq, edq, edq_max):
+    def edg_gen(elq, edq_max):
+        edg = list([0, 0, 0])
 
-    for i, k in enumerate(sorted(dct.keys())):
-        if i != 0 and i % step == 0:
-            print()
+        edg[0] = rand(1, elq)
+        edg[1] = rand(1, elq)
+        edg[2] = rand(1, edq_max)
 
-            for j in range(ss):
-                print(" ", end="")
+        return edg
 
-        print(str(k) + ": " + str(dct[k]),
-            end=", " if i < len(dct) - 1 else "")
-
-    print("}")
-
-def edges_gen(nmax, edq, wmax):
     edgs = list()
-    e = list([0, 0, 0])
 
-    for i in range(edq):
-        e[0] = rand(1, nmax)
-        e[1] = rand(1, nmax)
-        e[2] = rand(1, wmax)
-
-        edgs += [tuple(e)]
+    [edgs.append(tuple(edg_gen(elq, edq_max))) for i in range(edq)]
 
     return edgs
 
-def els_place(elq, lx, ly, dx, dy):
+def els_place(elq, dx, dy, lx, ly):
     els = list()
 
-    for i in range(elq):
-        els += [0]
-        #els[i] = (lx - dx - (i + 1) * dx, ly - dy)
-        els[i] = (dx * (elq - i) - dx, ly - dy)
+    [els.append((dx * (elq - i) - dx, ly - dy)) for i in range(elq)]
 
     return els
 
@@ -125,121 +113,124 @@ def elp_rand(elsp, bq):
 def els_scat(elq, bq):
     elsp = list()
 
-    for p in range(elq):
-        elsp += [elp_rand(elsp, bq)]
+    [elsp.append(elp_rand(elsp, bq)) for i in range(elq)]
 
     return elsp
 
+def crd_to_elp(crd, dx, dy, nx):
+    elp = crd[0] / dx + crd[1] / dy * nx + 1
+
+    return int(elp)
+
 def crd_calc(n, dx, dy, nx):
-    y = (n - 1) // nx # clean scheme's pos by Y (dy = 1)
-    x = n - y * nx - 1 # clean scheme's pos by X (dx = 1)
+    crd = tuple()
+    y = (n - 1) // nx # clean element's pos by Y (dy = 1)
+    x = n - y * nx - 1 # clean element's pos by X (dx = 1)
 
     x *= dx
     y *= dy
 
-    return (x, y)
+    crd = (x, y)
+
+    return crd
 
 def crds_calc(elsp, dx, dy, nx):
     crds = list()
 
-    if isinstance(elsp, int):
-        return crd_calc(elsp, dx, dy, nx)
-
-    for i in range(len(elsp)):
-        crds += [crd_calc(elsp[i], dx, dy, nx)]
+    [crds.append(crd_calc(elsp[i], dx, dy, nx)) for i in range(len(elsp))]
 
     return crds
 
-# NOTE: expensive
-def links_count(edgs):
-    links = dict()
+def edg_link_len(edg, crds):
+    ll = 0.0
+    n1 = edg[0] - 1
+    n2 = edg[1] - 1
+    w = edg[2]
 
-    for i in range(len(edgs)):
-        n2 = edgs[i][1]
+    x1, y1 = crds[n1][0], crds[n1][1]
+    x2, y2 = crds[n2][0], crds[n2][1]
 
-        if n2 in links:
-            links[n2] += 1
-        else:
-            links[n2] = 1
+    ll = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5 * w
 
-    return links
+    return ll
 
-def link_len_sum(edgs, crds):
+def edgs_links_len(edgs, crds, conf=None):
     ll = 0.0
 
-    for i in range(len(edgs)):
-        n1 = edgs[i][0] - 1
-        n2 = edgs[i][1] - 1
-        w = edgs[i][2]
-
-        x1 = crds[n1][0]
-        y1 = crds[n1][1]
-        x2 = crds[n2][0]
-        y2 = crds[n2][1]
-
-        ll += sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2) * w
+    for edg in edgs:
+        if conf == None:
+            ll += edg_link_len(edg, crds)
+        else:
+            if edg[0] - 1 == conf or edg[1] - 1 == conf:
+                ll += edg_link_len(edg, crds)
 
     return ll
 
 def mut_comp(edgs, elsp, dx, dy, nx, ny, comp_diagonal):
     crds = crds_calc(elsp, dx, dy, nx)
-    ll_min = link_len_sum(edgs, crds)
     left = 1
     right = nx * ny + 1
 
     for i in range(len(elsp)):
+        ll = edgs_links_len(edgs, crds, i)
+        deltas = dict()
+        j = 0
+
         if comp_diagonal:
             left = elsp[i] + 1
 
         for j in range(left, right):
             if j not in elsp:
-                ll = 0.0
+                delta = 0.0
 
-                crds[i] = crds_calc(j, dx, dy, nx)
+                crds[i] = crd_calc(j, dx, dy, nx)
+                delta = edgs_links_len(edgs, crds, i)
 
-                ll = link_len_sum(edgs, crds)
-                if ll < ll_min:
-                    elsp[i] = j
-                    ll_min = ll
-                else:
-                    crds[i] = crds_calc(elsp[i], dx, dy, nx)
+                if delta < ll:
+                    deltas[j] = delta
 
-    return elsp, crds
+        if deltas:
+            j = min(deltas, key=deltas.get)
+
+            elsp[i] = j
+            crds[i] = crd_calc(j, dx, dy, nx)
+
+    return elsp
 
 def mut_swap(edgs, elsp, dx, dy, nx):
     crds = crds_calc(elsp, dx, dy, nx)
     ll = 0.0
-    ll_min = link_len_sum(edgs, crds)
-    p1 = rand(0, len(elsp) - 1)
-    p2 = rand(0, len(elsp) - 1)
+    delta = 0.0
+    p = list([0, 0])
 
-    while p2 == p1:
-        p2 = rand(0, len(elsp) - 1)
+    for i in range(2):
+        p[i] = rand(0, len(elsp) - 1)
 
-    ll = link_len_sum(edgs, swap(crds, p1, p2))
-    if ll < ll_min:
-        elsp = swap(elsp, p1, p2)
-        crds = swap(crds, p1, p2)
+        while p.count(p[i]) > 1:
+            p[i] = rand(0, len(elsp) - 1)
 
-    return elsp, crds
+        ll += edgs_links_len(edgs, crds, p[i])
+
+    crds = swap(crds, p[0], p[1])
+
+    for i in range(2):
+        delta += edgs_links_len(edgs, crds, p[i])
+
+    if delta < ll:
+        elsp = swap(elsp, p[0], p[1])
+
+    return elsp
 
 def mut_sort(edgs, elsp, dx, dy, nx):
-    elsp_min = elsp[:]
     crds = crds_calc(elsp, dx, dy, nx)
-    crds_min = list()
-    ll = 0.0
-    ll_min = link_len_sum(edgs, crds)
+    ll = edgs_links_len(edgs, crds)
 
-    elsp_min.sort()
+    crds.sort()
 
-    crds_min = crds_calc(elsp_min, dx, dy, nx)
+    if edgs_links_len(edgs, crds) < ll:
+        elsp.sort()
 
-    ll = link_len_sum(edgs, crds_min);
-    if ll < ll_min:
-        elsp = elsp_min[:]
-        crds = crds_min[:]
-
-    return elsp, crds
+    return elsp
 
 def file_form(fname, edgs, crds, els):
     f = open(fname, "w")
@@ -273,21 +264,21 @@ def file_form(fname, edgs, crds, els):
         f.write("Z Axis Up" + "\n")
 
 def main():
-    parser, opts = _opts_get()
-    elq = opts.elq
-    dx = opts.dx
-    dy = opts.dy
-    nx = opts.nx
-    ny = opts.ny
-    edq = opts.edq
-    edq_max = opts.edq_max
-    its = opts.its
-    fname = opts.fname
-    with_sort = opts.with_sort
-    ll_show = opts.ll_show
-    with_swap = opts.with_swap
-    mode = opts.mode
-    comp_diagonal = opts.comp_diagonal
+    parser, args = _args_get()
+    elq = args.elq
+    dx = args.dx
+    dy = args.dy
+    nx = args.nx
+    ny = args.ny
+    edq = args.edq
+    edq_max = args.edq_max
+    its = args.its
+    fname = args.fname
+    with_swap = args.with_swap
+    with_sort = args.with_sort
+    ll_show = args.ll_show
+    mode = args.mode
+    comp_diagonal = args.comp_diagonal
     cc = 0
     ts = 0 # time start
     tf = 0
@@ -297,22 +288,13 @@ def main():
     els = list()
     edgs = list()
     elsp = list() # elements positions
-    elsp_min = list()
-    crds_min = list()
-    ll = 0.0
+    ll_start = 0.0
     ll_min = float("inf")
-    elp_prev = 0 # only for pedantic mode
+    times_opt = 0.0
+    delta = 0.0
+    perc = 0.0
 
-    if elq is None \
-            or dx is None \
-            or dy is None \
-            or nx is None \
-            or ny is None \
-            or edq is None \
-            or edq_max is None \
-            or its is None \
-            or fname is None \
-            or mode not in ["hard", "soft", "pedantic"]:
+    if mode not in ["hard", "soft"]:
         parser.print_help()
 
         sys.exit(-1)
@@ -321,28 +303,21 @@ def main():
     lx = dx * nx # field's size by X
     ly = dy * ny # field's size by Y
     bq = nx * ny
-
-    els = els_place(elq, lx, ly, dx, dy) # only for file forming
-    edgs = edges_gen(elq, edq, edq_max)
-    #links = links_count(edgs)
+    els = els_place(elq, dx, dy, lx, ly) # only for file forming
+    edgs = edgs_gen(elq, edq, edq_max)
     elsp = els_scat(elq, bq)
     crds = crds_calc(elsp, dx, dy, nx)
-    ll = link_len_sum(edgs, crds)
-
-    elp_prev = list([0, elsp[0]])
+    ll_start = edgs_links_len(edgs, crds)
 
     print("connectedness coefficient: " + str(round(cc, 3)) + "%" + "\n")
 
-    #print("edgs    : ", end="")
-    #list_print(edgs, ss=11)
-
-    #print("links   : ", end="")
-    #dict_print(links, step=6, ss=11)
+    print("edgs    : ", end="")
+    list_print(edgs, ss=11)
 
     print("elsp    : ", end="")
     list_print(elsp, step=12, ss=11)
 
-    print("ll      : " + str(round(ll, 3)))
+    print("ll      : " + str(round(ll_start, 3)))
 
     if ll_show:
         print()
@@ -350,64 +325,71 @@ def main():
     ts = time.time()
 
     for i in range(its):
-        ll_mut_min = 0.0
-
         # mutate acc
         while True:
-            elsp_min, crds_min = mut_comp(edgs, elsp, dx, dy, nx, ny,
+            ll = 0.0
+
+            elsp = mut_comp(edgs, elsp, dx, dy, nx, ny,
                 comp_diagonal)
 
             if with_swap:
-                elsp_min, crds_min = mut_swap(edgs, elsp, dx, dy, nx)
+                elsp = mut_swap(edgs, elsp, dx, dy, nx)
 
             if with_sort:
-                elsp_min, crds_min = mut_sort(edgs, elsp, dx, dy, nx)
+                elsp = mut_sort(edgs, elsp, dx, dy, nx)
 
-            ll_mut_min = link_len_sum(edgs, crds_min)
-            if ll_mut_min < ll_min:
-                ll_min = ll_mut_min
+            crds = crds_calc(elsp, dx, dy, nx)
+            ll = edgs_links_len(edgs, crds)
+            if ll < ll_min:
+                if ll_min < float("inf"):
+                    perc = round((ll_min - ll) / ll_min * 100, 2)
+                else:
+                    perc = round((ll_start - ll) / ll_start * 100, 2)
+
+                ll_min = ll
 
                 if ll_show:
-                    print(i + 1, round(ll_min, 3))
+                    print(i + 1, round(ll_min, 3), "(" + str(perc) + "%)")
             else:
                 break
+
+        if i == its - 1:
+            break
 
         # generate another acc
         if mode == "hard":
             elsp = els_scat(elq, bq)
         elif mode == "soft":
             elsp[rand(0, elq - 1)] = elp_rand(elsp, bq)
-        elif mode == "pedantic":
-            p = rand(0, elq - 1)
-
-            if ll_mut_min >= ll_min:
-                elsp[elp_prev[0]] = elp_prev[1]
-
-            elsp[p] = elp_rand(elsp, bq)
-            elp_prev = [p, elsp[p]]
-
-        crds = crds_calc(elsp, dx, dy, nx)
 
     tf = time.time() - ts
 
-    print("\n" + "--- generation completed ---")
+    print()
+    print("--- generation completed ---")
+    print()
 
-    print("\n" + "elsp_min: ", end="")
-    list_print(elsp_min, step=12, ss=11)
-
-    #print("crds_min: ", end="")
-    #list_print(crds_min, ss=11)
-
-    print("ll_min  : " + str(round(ll_min, 3)))
+    print("elsp    : ", end="")
+    list_print(elsp, step=12, ss=11)
 
     #print("els     : ", end="")
     #list_print(els, ss=11)
 
-    print("\n" + str(round(ll / ll_min, 3)) + " times optimized")
+    #print("crds    : ", end="")
+    #list_print(crds, ss=11)
 
-    print("\n" + "elapsed: " + str(round(tf, 6)) + "s")
+    print("ll_min  : " + str(round(ll_min, 3)))
 
-    file_form(fname, elsp, crds_min, els)
+    times_opt = round(ll_start / ll_min, 3)
+    delta = ll_start - ll_min
+    perc = round(delta / ll_start * 100, 2)
+
+    print()
+    print(str(times_opt) + " (" + str(perc) + "%) times optimized")
+
+    print()
+    print("elapsed: " + str(round(tf, 6)) + "s")
+
+    file_form(fname, elsp, crds, els)
 
 if __name__ == "__main__":
     main()
