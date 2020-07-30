da63985491efe598d622e6b40a91782e93a1163e
maximhesed <maximtemny@mail.ru>
2020-07-30 09:08:52 +0300

    some cosmetics

--- emulator.py
+++ emulator.py
@@ -20,9 +20,6 @@ def _args_get(args=sys.argv[1:]):
     parser.add_argument("fsx", type=int, help="field size by X")
     parser.add_argument("fsy", type=int, help="field size by Y")
     parser.add_argument("fname", type=str, help="file with TP-ELC commands")
-    parser.add_argument("--log-show", action="store_true", help="show log")
-
-    parser.set_defaults(log_show=False)
 
     args = parser.parse_args(args)
 
@@ -37,28 +34,27 @@ def file_read(fname):
 
     return cmds
 
-# place schemes
-def schemes_place(canvas, n, dx, dy, ly):
+def els_place(canvas, elq, dx, dy, ly):
     els = list()
     lbls = list()
+    ind = 0
 
-    for i in range(n):
+    for i in range(elq):
         els += [0]
         lbls += [0]
+        ind = elq - i
 
-        els[i] = canvas.create_rectangle(dx * (n - i) - dx, ly - dy,
-            dx * (n - i + 1) - dx, ly, outline="#000", fill="#1c1", width=2)
-        lbls[i] = canvas.create_text(dx * (n - i + 0.5) - dx, ly - dy / 2,
+        els[i] = canvas.create_rectangle(dx * ind - dx, ly - dy,
+            dx * (ind + 1) - dx, ly, outline="#000", fill="#1c1", width=2)
+        lbls[i] = canvas.create_text(dx * (ind + 0.5) - dx, ly - dy / 2,
             font=("Terminus", 8), text=str(i + 1))
 
     return els, lbls
 
-# manipulator init
 def man_init(canvas, r, dx, dy):
     hand = canvas.create_line(0, 0, dx / 2, dy / 2, width=6, fill="black")
     p = canvas.create_oval([0, 0],[r * 1.5, r * 1.5], fill="yellow")
-    v = canvas.create_oval([r / 2, r / 2], [r, r],
-        fill="blue")
+    v = canvas.create_oval([r / 2, r / 2], [r, r], fill="blue")
 
     return hand, p, v
 
@@ -72,7 +68,7 @@ def man_move(canvas, x, y, hand, p, v, vstate, els, eln, lbls):
 
     if abs(v_crds[0] - el_crds[0]) <= 1 \
             and abs(v_crds[1] - el_crds[1]) <= 1 \
-            and vstate == 1:
+            and vstate == True:
         canvas.move(els[0], x, y)
         canvas.move(lbls[eln], x, y)
 
@@ -87,7 +83,7 @@ def grid_create(canvas, lx, ly, dx, dy, w):
 
     canvas.grid(row=0, column=0)
 
-def cmds_parse(canvas, cmds, num, p, v, vstate, els, eln, p_crds_arr, log):
+def cmds_parse(canvas, cmds, num, p, v, vstate, els, eln, p_crds_arr):
     cmd = cmds[num][0]
     pflag = False
     p_crds = list([0, 0])
@@ -95,42 +91,36 @@ def cmds_parse(canvas, cmds, num, p, v, vstate, els, eln, p_crds_arr, log):
     if cmd == "Clear Points":
         p_crds_arr = list()
 
-        if log:
-            print(cmd)
+        print(cmd)
     elif cmd[0:19] == "Add Point Cartesian":
         raw = cmd[19:len(cmd)]
         crds = raw.split(",")
         p_crds_arr += [(float(crds[0]), float(crds[1]))]
 
-        if log:
-            print(cmd[0:19], int(crds[0]), int(crds[1]))
+        print(cmd[0:19], int(crds[0]), int(crds[1]))
     elif cmd == "Z Axis Up":
         canvas.itemconfig(p, fill="yellow")
 
-        if log:
-            print(cmd)
+        print(cmd)
     elif cmd == "Z Axis Down":
         canvas.itemconfig(p, fill="red")
         canvas.update()
 
-        if log:
-            print(cmd)
+        print(cmd)
 
         sleep(0.25)
     elif cmd == "Vacuum On":
         canvas.itemconfig(v, fill="black")
-        vstate = 1
+        vstate = True
         els[0] = els[eln]
 
-        if log:
-            print(cmd)
+        print(cmd)
     elif cmd == "Vacuum Off":
         canvas.itemconfig(v, fill="blue")
-        vstate = 0
+        vstate = False
         eln += 1
 
-        if log:
-            print(cmd)
+        print(cmd)
     elif cmd[0:13] == "Move To Point":
         pnum = int(cmd[14:len(cmd)]) - 1
         p_crds[0] = p_crds_arr[pnum][0]
@@ -138,13 +128,11 @@ def cmds_parse(canvas, cmds, num, p, v, vstate, els, eln, p_crds_arr, log):
 
         pflag = True
 
-        if log:
-            print(cmd[0:14], pnum + 1)
+        print(cmd[0:14], pnum + 1)
 
     return pflag, els, eln, p_crds, p_crds_arr, vstate
 
-# move element
-# NOTE: recursive
+# (recursive) move element
 def el_move(w, canvas, hand, p, v, vstate, p_crds, p_crds_arr, els, eln,
         lbls):
     crds = canvas.coords(v)
@@ -183,14 +171,13 @@ def main():
     fsx = args.fsx
     fsy = args.fsy + 1
     fname = args.fname
-    log = args.log_show;
     lx = dx * fsx # field's size by X
     ly = dy * fsy # field's size by Y
     hand = None
     p = None
     pr = 10 # pin's radius
     v = None
-    vstate = 0 # vacuum state
+    vstate = False # vacuum state
     eln = 0 # current element's number
     p_crds_arr = list()
     cmds = file_read(fname)
@@ -202,7 +189,7 @@ def main():
     w.title("TP-ELC emulator")
 
     grid_create(canvas, lx, ly, dx, dy, 2) # create grid
-    els, lbls = schemes_place(canvas, elq, dx, dy, ly) # place schemes
+    els, lbls = els_place(canvas, elq, dx, dy, ly) # place elements
     hand, p, v = man_init(canvas, pr, dx, dy) # manipulator init
     canvas.pack()
 
@@ -212,12 +199,13 @@ def main():
     # commands parse
     for i in range(len(cmds)):
         pflag, els, eln, p_crds, p_crds_arr, vstate = cmds_parse(canvas,
-            cmds, cmdn, p, v, vstate, els, eln, p_crds_arr, log)
+            cmds, cmdn, p, v, vstate, els, eln, p_crds_arr)
         cmdn += 1
 
         if pflag == True:
             el_move(w, canvas, hand, p, v, vstate, p_crds, p_crds_arr, els,
                 eln, lbls)
+
             pflag = False
 
     sleep(5)
